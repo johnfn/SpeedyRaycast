695b6105c90b17c16348d6234bec75f980d64dc1
Grant Mathews <johnfn@gmail.com>
2015-06-15 21:45:12 -0700

    More changes

--- Assets/LightCast.cs
+++ Assets/LightCast.cs
@@ -67,45 +67,6 @@ struct Line
     }
 }
 
-struct LightRay
-{
-    public Vector2 FirstPoint;
-
-    private Vector2 _secondPoint;
-    public Vector2 SecondPoint
-    {
-        get { return _secondPoint; }
-        set
-        {
-            HasSecondPoint = true;
-            _secondPoint = value;
-        }
-    }
-
-    public bool HasSecondPoint { get; private set; }
-
-    public Vector2 FurthestPoint
-    {
-        get { return HasSecondPoint ? SecondPoint : FirstPoint; }
-    }
-
-    public void DebugDraw(Vector2 start)
-    {
-        Debug.DrawLine(start, FurthestPoint, Color.yellow);
-        Debug.DrawLine(start, FirstPoint, Color.black);
-    }
-
-    public void DebugDrawColor(Vector2 start, Color color)
-    {
-        Debug.DrawLine(start, FurthestPoint, color);
-    }
-
-    public float Angle(Vector2 from)
-    {
-        return Mathf.Atan2(FurthestPoint.y - from.y, FurthestPoint.x - from.x);
-    }
-}
-
 public class LightCast : MonoBehaviour
 {
     public List<GameObject> gameobjectPool; // TODO rename
@@ -146,7 +107,7 @@ public class LightCast : MonoBehaviour
 	    {
 	        gameobjectPool[i].SetActive(true);
 
-	        DrawPolygon(polygons[i], gameobjectPool[i]);
+	        DrawTriangle(polygons[i], gameobjectPool[i]);
 	    }
 	}
 
@@ -158,7 +119,7 @@ public class LightCast : MonoBehaviour
         Debug.DrawLine(new Vector2(x, y - size), new Vector2(x, y + size), c);
     }
 
-    void CastLight()
+    private void CastLight()
     {
         var lightOrigin = new Vector2(transform.position.x, transform.position.y);
 
@@ -194,198 +155,95 @@ public class LightCast : MonoBehaviour
         // Sort points radially, counterclockwise.
 
         vertices.Sort((point1, point2) =>
-	    {
+        {
             var angle1 = Mathf.Atan2(point1.y - transform.position.y, point1.x - transform.position.x);
             var angle2 = Mathf.Atan2(point2.y - transform.position.y, point2.x - transform.position.x);
 
             return angle1.CompareTo(angle2);
+        });
 
-	    });
+        // Bucket vertices by angle.
 
-        var rays = new List<LightRay>();
+        var groupedVertices =
+            vertices
+                .GroupBy(vert => Mathf.Atan2(vert.y - transform.position.y, vert.x - transform.position.x))
+                .Select(list => list.OrderBy(vert => Vector2.Distance(vert, lightOrigin)).ToList())
+                .ToList();
 
-        foreach (var vertex in vertices)
+        var groupedVisibleVerticies = new List<List<Vector2>>();
+
+        // Transform vertex group list into *visible* vertex group list.
+
+        foreach (var vertexGroup in groupedVertices)
         {
-            var rayDirection = (vertex - lightOrigin).normalized; // It is important to normalize the direction - otherwise, Unity's raycasting will go into lines if it is too large.
-            var hit = Physics2D.Raycast(transform.position, rayDirection);
-            var castedLine = new Line {Start = transform.position, End = hit.point};
+            var visibleVerticies = new List<Vector2>();
 
-            if (hit.collider != null)
+            var raycastingPath = new List<Vector2> {lightOrigin};
+            raycastingPath.AddRange(vertexGroup);
+
+            var rayDirection = (raycastingPath[1] - raycastingPath[0]).normalized;
+
+            for (var j = 0; j < raycastingPath.Count() - 1; j++)
             {
-                var result = new LightRay();
+                var start = raycastingPath[j];
+                var targetVertex = raycastingPath[j + 1];
 
-                if (hit.point != vertex)
+                var hit = Physics2D.Raycast(start + rayDirection * 0.01f, rayDirection);
+
+                if (hit.collider == null)
                 {
-                    // Unity's raycasting made us go straight through the vertex and had us collide with the wall after the vertex.
-
-                    if (Vector2.Distance(lightOrigin, hit.point) > Vector2.Distance(lightOrigin, vertex))
-                    {
-                        result.FirstPoint = vertex;
-                        result.SecondPoint = hit.point;
-                    }
-                    else
-                    {
-                        var success = false;
-
-                        // TODO: COPY AND PASTED CODE LOL
-
-                        var adjacentLines = lines.FindAll(l => l.Start == vertex || l.End == vertex);
-
-                        foreach (var line in adjacentLines)
-                        {
-                            if (line.LineContainsPoint(castedLine.Start) && line.LineContainsPoint(castedLine.End))
-                            {
-                                // Continue the raycast from the other side
-
-                                var otherSide = line.Start == vertex ? line.End : line.Start;
-                                var nextHit = Physics2D.Raycast(otherSide + rayDirection * 0.01f, rayDirection);
-
-                                result.SecondPoint = nextHit.point;
-
-                                success = true;
-                            }
-                        }
-
-                        // We collided before we hit the vertex, so we don't need to consider this one.
-                        if (!success)
-                        {
-                            continue;
-                        }
-                    }
-                }
-                else
-                {
-                    result.FirstPoint = hit.point;
-
-                    // Check to see if we're at a corner that we can continue on past.
-
-                    var nextHit = Physics2D.Raycast(vertex + rayDirection * .01f, rayDirection);
-
-                    if (Vector2.Distance(nextHit.point, hit.point) > .02f)
-                    {
-                        result.SecondPoint = nextHit.point;
-                    }
-                    else
-                    {
-                        // Check if we're right on top of a wall. In that case, simply continue the raycast on the other side of the wall.
-
-                        // Find the 2 vertices adjacent to the current vertex. We do this by looking through our line collection. One of these lines
-                        // will start with this vertex, and one will end with it.
-
-                        var adjacentLines = lines.FindAll(l => l.Start == vertex || l.End == vertex);
-
-                        foreach (var line in adjacentLines)
-                        {
-                            if (line.LineContainsPoint(castedLine.Start) && line.LineContainsPoint(castedLine.End))
-                            {
-                                // Continue the raycast from the other side
-
-                                var otherSide = line.Start == vertex ? line.End : line.Start;
-
-                                nextHit = Physics2D.Raycast(otherSide + rayDirection * 0.01f, rayDirection);
-
-                                result.SecondPoint = nextHit.point;
-                            }
-                        }
-
-                    }
+                    return; //TODO heh
                 }
 
-                // Draw debugging stuff. 
+                var madeItToTheNextVertex = MyUtil.CloseEnough4Me(hit.point.x, targetVertex.x) && MyUtil.CloseEnough4Me(hit.point.y, targetVertex.y);
 
-                /*
-                if (result.HasSecondPoint)
+                // If we are running directly on top of any edge, don't allow it to stop us.
+                if (lines.Exists(l => l.Start == start && l.End == targetVertex || l.End == start && l.Start == targetVertex))
                 {
-                    Debug.DrawLine(transform.position, result.SecondPoint, Color.red);
+                    madeItToTheNextVertex = true;
                 }
 
-                Debug.DrawLine(transform.position, result.FirstPoint, Color.black);
-                */
+                // On a corner, Unity's raycasting may have us go straight through the vertex and collide with something after the vertex.
+                if (Vector2.Distance(lightOrigin, hit.point) > Vector2.Distance(lightOrigin, targetVertex))
+                {
+                    madeItToTheNextVertex = true;
+                }
 
-                rays.Add(result);
+                if (!madeItToTheNextVertex) break;
+
+                visibleVerticies.Add(targetVertex);
             }
-            else
-            {
-                Debug.Log("No collider?");
-            }
-        }
 
-        var raysGroupedByAngle = new List<List<LightRay>> {new List<LightRay> {rays[0]}};
+            if (visibleVerticies.Count == 0) continue;
 
-        for (var i = 1; i < rays.Count; i++)
-        {
-            var currentRayGroup = raysGroupedByAngle.Last();
-            var ray = rays[i];
-            var rayAngle = ray.Angle(lightOrigin);
+            groupedVisibleVerticies.Add(visibleVerticies);
 
-            if (Mathf.Approximately(currentRayGroup[0].Angle(lightOrigin), rayAngle))
+            // Check if we're on a vertex which is a corner, and attempt to go past it.
+
+            var finalHit = Physics2D.Raycast(visibleVerticies.Last() + rayDirection * .01f, rayDirection);
+
+            if (Vector2.Distance(finalHit.point, visibleVerticies.Last()) > .02f)
             {
-                currentRayGroup.Add(ray);
-            }
-            else
-            {
-                raysGroupedByAngle.Add(new List<LightRay> {ray});
+                visibleVerticies.Add(finalHit.point);
             }
         }
 
         polygons.Clear();
 
-        for (var i = 0; i < raysGroupedByAngle.Count(); i++)
-        {
-            if (raysGroupedByAngle[i].Count != 1)
-            {
-                Debug.Log(raysGroupedByAngle[i].Count);
-            }
-
-            var group = raysGroupedByAngle[i]
-                .OrderBy(x => Vector2.Distance(lightOrigin, x.FurthestPoint));
-
-            var nextGroup = raysGroupedByAngle[(i + 1) % raysGroupedByAngle.Count()]
-                .OrderBy(x => Vector2.Distance(lightOrigin, x.FurthestPoint));
-
-            foreach (var r1 in group)
-            {
-                foreach (var r2 in nextGroup)
-                {
-                    var bound = FindTriangleBound(r1, r2, lines);
-
-                    if (bound == default(Line)) continue;
-
-                    polygons.Add(new List<Vector2>
-                    {
-                        lightOrigin,
-                        bound.Start,
-                        bound.End
-                    });
-
-                    goto Done;
-                }
-            }
-
-            foreach (var r1 in group) r1.DebugDraw(lightOrigin);
-            foreach (var r1 in nextGroup) r1.DebugDraw(lightOrigin);
-
-            Done:
-            ;
-
-        }
-
         /*
-        for (var i = 0; i < rays.Count; i++)
+        for (var i = 0; i < groupedVisibleVerticies.Count; i++)
         {
-            var ray = rays[i];
-            var nextRay = rays[(i + 1) % rays.Count];
-            var bound = FindTriangleBound(ray, nextRay, lines);
+            var group = groupedVisibleVerticies[i];
+            var nextGroup = groupedVisibleVerticies[(i + 1) % groupedVisibleVerticies.Count];
+
+            var bound = FindTriangleBound(group, nextGroup, lines);
 
             if (bound == default(Line))
             {
-                Debug.Log("Bad");
-                Debug.Log(i);
-                Debug.Log(transform.position.x);
-                Debug.Log(transform.position.y);
+                Debug.Log("Couldn't bound triangle!");
 
-                ray.DebugDraw(transform.position);
-                nextRay.DebugDraw(transform.position);
+                Debug.DrawLine(lightOrigin, group.Last(), Color.red);
+                Debug.DrawLine(lightOrigin, nextGroup.Last(), Color.red);
             }
 
             polygons.Add(new List<Vector2>
@@ -396,121 +254,58 @@ public class LightCast : MonoBehaviour
             });
         }
         */
+
+        polygons.Add(new List<Vector2>
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1)
+
+        });
     }
 
-    /*
-    bool DoRaysOvelap(LightRay r1, LightRay r2)
+    Line FindTriangleBound(List<Vector2> group, List<Vector2> nextGroup, List<Line> lines)
     {
-        var l1 = new Line {Start = lightOrigin, End = r1.FurthestPoint};
-        var l2 = new Line {Start = lightOrigin, End = r2.FurthestPoint};
-
-        var shortestLine = l1.Length > l2.Length ? l2 : l1;
-        var longestLine = l1.Length > l2.Length ? l1 : l2;
-
-        if (longestLine.SegmentContainsPoint(shortestLine.End))
+        foreach (var v1 in group)
         {
-            overlappedLines.Add(shortestLine.End == r1.FurthestPoint ? r1 : r2);
-        }
-    }
-    */
-
-    Line FindTriangleBound(LightRay ray, LightRay nextRay, List<Line> lines)
-    {
-        var potentialBounds = new List<Line>
-        {
-            new Line {Start = ray.FirstPoint,    End = nextRay.FirstPoint},
-            new Line {Start = ray.FurthestPoint, End = nextRay.FirstPoint},
-            new Line {Start = ray.FirstPoint,    End = nextRay.FurthestPoint},
-            new Line {Start = ray.FurthestPoint, End = nextRay.FurthestPoint}
-        };
-
-        foreach (var potentialBound in potentialBounds)
-        {
-            if (lines.Exists(l => l.SegmentContainsPoint(potentialBound.Start) && l.SegmentContainsPoint(potentialBound.End)))
+            foreach (var v2 in nextGroup)
             {
-                return potentialBound;
+                if (lines.Exists(l => l.SegmentContainsPoint(v1) && l.SegmentContainsPoint(v2)))
+                {
+                    return new Line {Start = v1, End = v2};
+                }
             }
         }
 
         return default(Line);
     }
-
-    void DrawPolygon(List<Vector2> nodePositions, GameObject obj)
+    
+    /*
+        DrawTriangle - takes a list of coordinates for a triangle, and a GameObject obj that will 
+        render it, and creates the mesh on that GameObject.
+    */
+    void DrawTriangle(List<Vector2> nodePositions, GameObject obj)
     {
-        //Components
-        var mf = obj.GetComponent<MeshFilter>();
-        mf.mesh.Clear();
+        var meshFilter = obj.GetComponent<MeshFilter>();
+        var meshRenderer = obj.GetComponent<MeshRenderer>();
 
-        var mr = obj.GetComponent<MeshRenderer>();
-
-        //Create mesh
-        var mesh = CreateMesh(nodePositions);
-
-        //Assign materials
-        mr.material = GenericMaterial;
-
-        //Assign mesh to game object
-        mf.mesh = mesh;
+        meshRenderer.material = GenericMaterial;
+        meshFilter.mesh = CreateMesh(nodePositions);
     }
 
     Mesh CreateMesh(List<Vector2> nodes)
     {
-        int x;
-
-        //Create a new mesh
-        var mesh = new Mesh();
-
-        //Vertices
-        var vertex = new Vector3[nodes.Count];
-
-        for (x = 0; x < nodes.Count; x++)
+        var mesh = new Mesh
         {
-            vertex[x] = nodes[x];
-        }
+            vertices = new Vector3[] {nodes[0], nodes[1], nodes[2]},
+            uv = new[] {new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0)},
+            triangles = new[] {0, 2, 1}
+        };
 
-        //UVs
-        var uvs = new Vector2[vertex.Length];
-
-        for (x = 0; x < vertex.Length; x++)
-        {
-            if ((x % 2) == 0)
-            {
-                uvs[x] = new Vector2(0, 0);
-            }
-            else
-            {
-                uvs[x] = new Vector2(1, 1);
-            }
-        }
-
-        //Triangles
-        var tris = new int[3 * (vertex.Length - 2)];
-
-        var c1 = 0;
-        var c2 = vertex.Length - 1;
-        var c3 = vertex.Length - 2;
-
-        for (x = 0; x < tris.Length; x += 3)
-        {
-            tris[x] = c1;
-            tris[x + 1] = c2;
-            tris[x + 2] = c3;
-
-            c2--;
-            c3--;
-        }
-
-        //Assign data to mesh
-        mesh.vertices = vertex;
-        mesh.uv = uvs;
-        mesh.triangles = tris;
-
-        //Recalculations
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
         mesh.Optimize();
 
-        //Return the mesh
         return mesh;
     }
 
